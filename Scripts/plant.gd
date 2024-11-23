8512363107725a83a15fadb99a9493cb345c2da9
noodle-strudel <yollaine369@gmail.com>
2024-11-23 15:03:21 -0800

    Added prototype plants, hose, and sky images and scenes. Added signal for the plant on death. Added first plant resource.

---
+++ Scripts/plant.gd
@@ -0,0 +1,25 @@
+extends StaticBody2D
+
+signal on_plant_death(negate_score)
+@export var stats: Resource
+var health = 100
+var alive = true
+
+# Replaces sprite with sprite in the resources
+func _ready() -> void:
+	if stats:
+		$Sprite2D.texture = stats.sprite
+		alive = stats.alive
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if alive:
+		health = clamp(health - stats.decay, 0, 100)
+		$ProgressBar.value = health
+		if health <= 0:
+			alive = false
+			emit_signal("on_plant_death", 10)
+			print("signal sent, 10 score negated")
+
+func add_health(amount):
+	health = clamp(health + amount, 0, 100)
